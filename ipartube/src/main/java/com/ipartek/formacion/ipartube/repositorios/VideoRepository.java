aae353af9310f9a82501bd54215b02e41ac2fc0a
Desarrollo <Desarrollo@192.168.0.45>
2025-11-04 10:49:15 +0100

    Repositorios

---
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/repositorios/VideoRepository.java
@@ -0,0 +1,9 @@
+package com.ipartek.formacion.ipartube.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ipartek.formacion.ipartube.entidades.Video;
+
+public interface VideoRepository extends CrudRepository<Video, Long> {
+
+}
